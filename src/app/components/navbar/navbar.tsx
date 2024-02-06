fbc3be99414bbdd83457166aaf8dd04a6106cc61
Zach Donnelly <zachary.donnelly@walmart.com>
2024-02-05 20:12:20 -0500

    feat: navbar

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,20 +1,26 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import './navbar.scss';
 
 const Navbar: React.FC = () => (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/login">Login</Link>
-        </li>
-        <li>
-          <Link href="/signup">Sign up</Link>
-        </li>
-      </ul>
-    </nav>
+  <nav>
+    <div className="logo">
+      <Link href="/">
+        <Image src="/logo.svg" width={100} height={85} alt="Notebook" />
+      </Link>
+    </div>
+    <ul className="nav_items">
+      <li className="nav_item">
+        <Link href="/">Home</Link>
+      </li>
+      <li className="nav_item">
+        <Link href="/login">Login</Link>
+      </li>
+      <li className="nav_item">
+        <Link href="/signup">Sign up</Link>
+      </li>
+    </ul>
+  </nav>
 );
 
 export default Navbar;
