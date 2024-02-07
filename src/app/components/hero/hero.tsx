743e8e501dc542aa180d6811df26452ab30aed22
Zach Donnelly <zachary.donnelly@walmart.com>
2024-02-06 19:03:54 -0500

    feat: update homepage UI

--- src/app/components/hero/hero.tsx
+++ src/app/components/hero/hero.tsx
@@ -3,7 +3,8 @@ import './hero.scss';
 
 const Hero: React.FC = () => (
   <section className="hero">
-    <div className="hero_content">
+    <div className="hero_wrapper">
+      <div className="hero_img" />
       <header>
         <h1>
           Take notes,
@@ -13,10 +14,10 @@ const Hero: React.FC = () => (
           Experience real time translation into your specified language as you type. Listen to highlighted sections
           spoken out loud in your desired language!
         </h2>
+        <button type="button" className="hero_button">
+          <Link href="/signup">Get started now</Link>
+        </button>
       </header>
-      <button type="button" className="hero_button">
-        <Link href="/signup">Sign up now</Link>
-      </button>
     </div>
   </section>
 );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
-import Hero from './components/hero/hero';
+import Card from '@/components/cta/card/card';
+import Hero from '@/components/hero/hero';
+import Featured from './components/cta/featured/featured';
 import './styles/index.scss';
 
 const Home: React.FC = () => (
   <div className="home">
     <Hero />
+    <section className="cta">
+      <header>
+        <h3>Why Notebook?</h3>
+        <p>
+          Notebook is a personal note taking app and blog, all in one It&apos;s the perfect tool for creators, writers,
+          developers, and anyone who wants to share their ideas with the world.
+        </p>
+      </header>
+      <div className="card_row">
+        <Card
+          header="All the features you need"
+          content="Use Notebook's rich text editor, translation and text to voice"
+        />
+        <Card
+          header="Instant translations"
+          content="Watch your typed text become instantly translated into your desired language"
+        />
+        <Card
+          header="Text to voice translations"
+          content="Aside from our text translation functionality. Listen to your highlighted text in your desired language"
+        />
+      </div>
+    </section>
+    <section className="featured">
+      <header className="featured_header">
+        <h3>Featured blog posts</h3>
+        <p>Most recent blog posts that you may find interesting</p>
+      </header>
+      <div className="featured_row">
+        <Featured content="Building a more inclusive design community" />
+        <Featured content="Building a more inclusive design community" />
+        <Featured content="Building a more inclusive design community" />
+      </div>
+    </section>
   </div>
 );
 
